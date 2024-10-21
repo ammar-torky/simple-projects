e530d2a0d17bf0a638d75350399f73fbcf8d7f2c
Ammar <ammar.torky28@gmail.com>
2024-10-21 12:34:23 +0300

    new edit

--- 8-rock paper scissor.py
+++ 8-rock paper scissor.py
@@ -1,11 +1,10 @@
 import random
 choices = ("rock","paper","scissor")
 player_choice = None
-computer_choice = random.choice(choices)
-print(f"")
 
 while True :
     player_choice = input("Enter Your Choice ")
+    computer_choice = random.choice(choices)
     ########
     ##
     print(f"your choice is {player_choice}\ncomputer choice is {computer_choice}")
@@ -15,11 +14,13 @@ while True :
         print("Draw😒")
     elif player_choice == "rock" and computer_choice == "scissor":
         print("You Win😉")
+    elif player_choice =="scissor" and computer_choice == "paper":
+        print("You Win😉")
     elif player_choice == "paper" and computer_choice == "rock":
         print("You win😉")
     else:
         print("sorry, You lose🥲")
     
     quit_flag = input("\ndo you wanna compelete ? (y/n) : ".lower())
-    if quit_flag == "n":
+    if not quit_flag == "y":
         break
